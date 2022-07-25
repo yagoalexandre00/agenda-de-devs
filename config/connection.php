2c1dc58158c7f19129090c95b8b855b12372fa0e
Yago Goltara <goltarayago@gmail.com>
2022-07-25 16:48:53 -0300

    Creating database connection - PDO

---
+++ config/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+    $host = "127.0.0.1";
+    $dbname = "agenda";
+    $user = "admin";
+    $pass = "12345678";
+
+    try{
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+        // Ativar modo de debug de erros
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch(PDOException $e) {
+        // Erro na conexão
+        $error = $e->getMessage();
+        echo "Error: $error";
+    }
+
+?>
