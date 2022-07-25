5862dd011699c51503274cf4dc1150d48acb7f1c
Yago Goltara <goltarayago@gmail.com>
2022-07-25 16:06:23 -0300

    Setup configuration

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    include_once("config/url.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Agenda de Devs</title>
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.0/css/bootstrap.min.css" integrity="sha512-XWTTruHZEYJsxV3W/lSXG1n3Q39YIWOstqvmFsdNEEQfHoZ6vm6E9GK2OrF6DSJSpIbRbi+Nn0WDPID9O7xB2Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+</head>
+<body>
+    <h1>Testando agenda!</h1>
+    <i class="fas fa-eye"></i>
+</body>
+</html>
