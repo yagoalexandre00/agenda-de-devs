ca212194118f7078eabe52f3e647201ef32d5f70
Yago Goltara <goltarayago@gmail.com>
2022-07-25 21:46:59 -0300

    Adding View contact mode

--- config/process.php
+++ config/process.php
@@ -5,10 +5,26 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
-    
-    $query = "SELECT * FROM contacts";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
+    if(!empty($_GET)){
+        $id = $_GET['id'];
+    } 
 
-    $contacts = $stmt->fetchAll();
+    // Retorna dados de apenas um contato
+    if(!empty($id)){
+        $query = "SELECT * FROM contacts WHERE id = :id";
+
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+    } else {
+        $contacts = [];
+        
+        $query = "SELECT * FROM contacts";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+    }
+    
--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                         <td scope="row"><?= $contact['name'] ?></td>
                         <td scope="row"><?= $contact['phone'] ?></td>
                         <td class="actions">
-                            <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                            <a href="<?= $BASE_URL ?>show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
                             <a href="#"><i class="far fa-edit edit-icon"></i></a>
                             <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
                         </td>
