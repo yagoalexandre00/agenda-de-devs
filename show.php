b282cb066372afe50bbe4eb2fb827a78492e790f
Yago Goltara <goltarayago@gmail.com>
2022-07-25 22:04:49 -0300

    Creating a back button

--- show.php
+++ show.php
@@ -3,6 +3,7 @@
 ?>
 
 <div class="container" id="view-contact-container">
+    <?php include_once("templates/backbtn.html"); ?>
     <h1 id="main-title"><?= $contact['name'] ?></h1>
     <p class="bold">Telefone:</p>
     <p><?= $contact['phone']?></p>
