71ad01288273b9af586cda716b5e53f399c79a8c
Yago Goltara <goltarayago@gmail.com>
2022-07-25 21:06:16 -0300

    Creating and editing main page

--- config/process.php
+++ config/process.php
@@ -5,6 +5,8 @@
     include_once("connection.php");
     include_once("url.php");
 
+    $contacts = [];
+    
     $query = "SELECT * FROM contacts";
     $stmt = $conn->prepare($query);
     $stmt->execute();
--- index.php
+++ index.php
@@ -2,8 +2,42 @@
     include_once("templates/header.php");
 ?>
 
-<h1>Testando agenda!</h1>
-<i class="fas fa-eye"></i>
+<div class="container">
+    <?php if(isset($printMsg) && $printMsg != ''): ?>
+        <p id="msg"><?= $printMsg ?></p>
+    <?php endif; ?>
+
+    <h1 id="main-title">Minha Agenda</h1>
+    <?php if(count($contacts) > 0): ?>
+        
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($contacts as $contact): ?>
+                    <tr>
+                        <td scope="row"><?= $contact['id'] ?></td>
+                        <td scope="row"><?= $contact['name'] ?></td>
+                        <td scope="row"><?= $contact['phone'] ?></td>
+                        <td class="actions">
+                            <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                            <a href="#"><i class="far fa-edit edit-icon"></i></a>
+                            <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p id="empty-list-text">Ainda não há contatos, <a href="<?= $BASE_URL ?>create.php">adicione clicando aqui</a>!</p>
+    <?php endif; ?>
+</div>
 
 <?php
     include_once("template/footer.php")
