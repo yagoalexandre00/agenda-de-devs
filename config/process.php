12ccbaa245a2508696b33d1e783a8978164755a9
Yago Goltara <goltarayago@gmail.com>
2022-07-26 23:11:17 -0300

    Finished edit page

--- config/process.php
+++ config/process.php
@@ -27,7 +27,33 @@
                 $error = $e->getMessage();
                 echo "Error: $error";
             }
+
+        } else if ($data['type'] === 'edit'){
+            $name = $data['name'];
+            $phone = $data['phone'];
+            $observations = $data['observations'];
+            $id = $data['id'];
+
+            $query = "UPDATE contacts
+             SET name = :name, phone = :phone, observations = :observations
+             WHERE id = :id";
+
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+
+            try {
+                $stmt->execute();
+                $_SESSION['msg'] = "Contato atualizado com sucesso!";
+            } catch(PDOException $e) {
+                // Erro na conexão
+                $error = $e->getMessage();
+                echo "Error: $error";
+            }
         }
+
         header("Location:" . $BASE_URL . "../index.php");
     } else {    // SELEÇÃO DE DADOS
 
